25685752289f53054d5f268b937123717860797d
Alex <s200823@sggw.edu.pl>
2024-01-09 09:59:00 +0100

    Implemented ToDoList application, Text Analyzer and Weather Checker methods

--- Task-Advanced.py
+++ Task-Advanced.py
@@ -1,33 +1,34 @@
 import datetime
+import os
+import requests
 
-
-def PalindromeCheker():
-    str = input("Enter a string : ")
+def palindromeCheker():
+    string = input("Enter a string : ")
     clean_text = ''
-    for char in str :
+    for char in string:
         if char.isalnum():
             clean_text += char.lower()
 
     return clean_text == clean_text[::-1]
 
-# print(PalindromeCheker())
+# print(palindromeCheker())
 
-def IsPrime(num):
+def isPrime(num):
     if num < 1:
         return False
-    for i in range(2,int(num ** 1/2) + 1):
+    for i in range(2, int(num ** 1/2) + 1):
         if num % i == 0:
             return False
     return True
 
 def primeNumberFinder():
     numbers = int(input("Enter the number of prime numbers : "))
-    primes = [num for num in range(2,numbers+1) if IsPrime(num)]
+    primes = [num for num in range(2, numbers+1) if isPrime(num)]
     return primes
 
 # print(primeNumberFinder())
 
-def LogBook():
+def logBook():
     action = input("Do you want to 'write' or 'display' ? ")
 
     if action == "write":
@@ -44,8 +45,54 @@ def LogBook():
 # LogBook()
 
 
+def toDoListApp():
+    tasks = []
+    action = input("Choose action ? (add/mark_done/list) ")
 
+    if action == "add":
+        toDo = input("Add a todo item : ")
+        tasks.append({'task': toDo, 'done': False})
+        with open('ToDoList.txt', 'a') as file:
+            file.write(f'{tasks[0]["task"]} - {tasks[0]["done"]}')
 
+    if action == "mark_done":
+        task_num = int(input('Enter task number : '))
+        tasks[task_num]['done'] = True
+        with open('ToDoList.txt', 'a') as file:
+            file.write(f'{tasks[task_num]["task"]}')
 
+    if action == "list":
+        with open('ToDoList.txt', 'r') as file:
+            for task in file:
+                print(task.strip())
 
+# toDoListApp()
 
+def textAnalyzer(path):
+    with open(path, "r") as file:
+        text = file.read()
+        words_count = len(text.split())
+        sentence_count = text.count('.')
+        words = text.split()
+        word_length = [len(word) for word in words]
+        average_word_length = sum(word_length)/len(word_length)
+
+        print(f' Word_count: {words_count}\n '
+              f'sentence_count: {sentence_count}\n'
+              f' average_word_length: {average_word_length}')
+
+# file_path = 'sample_text'
+# textAnalyzer(file_path)
+
+def weatherCheker(city):
+    api_key = os.getenv('API_KEY')
+    base_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(base_url)
+    data = response.json()
+    if response.status_code == 200:
+        print(f'Weather in {city} : {data["weather"][0]["description"]} (temp is {data["main"]["temp"]})')
+    else:
+        print(f'Failed to fetch weather inforamtion. {response}')
+
+city = 'Warsaw'
+weatherCheker(city)
